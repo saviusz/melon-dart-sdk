f7c760b05ba76583856410eb454a61fef9a96fab
Seweryn Woliński <42438445+saviusz@users.noreply.github.com>
2026-03-22 00:22:20 +0100

    chore: remove spec dependency

--- test/song_service_test.dart
+++ test/song_service_test.dart
@@ -1,32 +1,42 @@
 import 'package:melon_sdk/index.dart';
 import 'package:melon_sdk/song/expections.dart';
-import 'package:spec/spec.dart';
-
+import 'package:test/test.dart';
 
 void main() {
+  late ArtistService artistsService;
+  late SongService songService;
+
+  setUp(() {
+    artistsService = ArtistService();
+    songService = SongService(artists: artistsService);
+  });
+
+  tearDown(() async {
+    await songService.dispose();
+  });
+
   test("Create song", () async {
+    final title = "Title";
 
-    final artistService = ArtistService();
-    final songService = SongService(artists: artistService);
+    expectLater(
+        songService.viewSongs(),
+        emitsInOrder([
+          [isA<Song>().having((song) => song.title, "title", title)]
+        ]));
 
-    final songId = await songService.createSong(title: "Title");
-    expect(songId).toBeTruthy();
+    final songId = await songService.createSong(title: title);
+    expect(songId, isNotEmpty, reason: "Song id should not be empty");
 
     final songs = await songService.getSongs();
-    expect(songs).contains(new Song(id: songId, title: "Title"));
-
+    expect(songs, contains(new Song(id: songId, title: title)));
   });
 
   test("Drop without title", () async {
-
-    final artistService = ArtistService();
-    final songService = SongService(artists: artistService);
-
     final request = songService.createSong(title: "");
-    expect(request).throws.isA<EmptySongTitleException>();
+
+    expect(request, throwsA(isA<EmptySongTitleException>()));
 
     final songs = await songService.getSongs();
-    expect(songs).toHaveLength(0);
-
+    expect(songs, isEmpty);
   });
 }
